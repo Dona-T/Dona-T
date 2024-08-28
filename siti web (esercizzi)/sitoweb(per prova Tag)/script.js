c9fe51e2215fd9aa7eef6dd8292a82ec4366746d
Dona-T <tommaso.nft04@gmail.com>
2024-08-28 16:18:27 +0200

    Add files via upload

---
+++ siti web (esercizzi)/sitoweb(per prova Tag)/script.js
@@ -0,0 +1,11 @@
+function getColor() {
+    console.log(document.getElementById("inputcolor").value)
+}
+
+function getValore() {
+    console.log(document.getElementById("inputTesto").value)
+}
+
+function cambiaValore() {
+    console.log(getElementById("progress").value)
+}
